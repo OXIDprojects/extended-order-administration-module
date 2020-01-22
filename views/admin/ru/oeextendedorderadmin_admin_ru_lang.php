23d00dc939c2a4c0ae9ca52eafadd523995eb04f
kermie <info@marco-steinhaeuser.de>
2020-01-22 10:06:59 +0100

    added RU and UK translations by @Pianoservice originally provided here https://forum.oxid-esales.com/t/oxid-6/96185/4

---
+++ views/admin/ru/oeextendedorderadmin_admin_ru_lang.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is part of OXID eSales Extended Order Admin module.
+ *
+ * OXID eSales Extended Order Admin module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales Extended Order Admin module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales Extended Order Admin module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category      module
+ * @package       extendedorderadmin
+ * @author        OXID eSales AG
+ * @link          http://www.oxid-esales.com/
+ * @copyright     (C) OXID eSales AG 2003-2015
+ */
+
+
+$sLangName = 'Ru';
+
+$aLang = array(
+    'charset' => 'UTF-8',
+    'oeextendedorderadmin' => 'OE Расширеный закз Админ',
+
+    // Order List
+    'OEEXTENDEDORDERADMIN_OXORDERDATE'                                  => 'Дата',
+    'OEEXTENDEDORDERADMIN_HEADER'                                       => 'Сумма заказа',
+    'OEEXTENDEDORDERADMIN_OXARTNUM'                                     => 'Арт.No.',
+    'OEEXTENDEDORDERADMIN_SUM'                                          => 'Количество',
+    'OEEXTENDEDORDERADMIN_OXTITLE'                                      => 'Описание',
+
+    // Order package
+    'OEEXTENDEDORDERADMIN_TITLE'                                      => 'OXID PACKLIST',
+    'OEEXTENDEDORDERADMIN_SHOWPACKLIST'                               => 'Печать упаковочного листа',
+    'OEEXTENDEDORDERADMIN_PACKLIST'                                   => 'УПАКОВОЧНЫЕ ЛИСТЫ',
+    'OEEXTENDEDORDERADMIN_FON'                                        => 'Телефон',
+    'OEEXTENDEDORDERADMIN_FAX'                                        => 'Fax',
+    'OEEXTENDEDORDERADMIN_REMARK'                                     => 'Примечание',
+    'OEEXTENDEDORDERADMIN_ORDERNR1'                                   => 'No.',
+    'OEEXTENDEDORDERADMIN_ORDERNR2'                                   => 'Заказано ',
+    'OEEXTENDEDORDERADMIN_MENUITEM'                                   => 'Администрирование заказов',
+    'OEEXTENDEDORDERADMIN_MENUSUBITEM'                                => 'Упаковочный лист',
+    'OEEXTENDEDORDERADMIN_DETAILS'                                    => 'Детали',
+);
---
+++ views/admin/uk/oeextendedorderadmin_admin_uk_lang.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is part of OXID eSales Extended Order Admin module.
+ *
+ * OXID eSales Extended Order Admin module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales Extended Order Admin module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales Extended Order Admin module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category      module
+ * @package       extendedorderadmin
+ * @author        OXID eSales AG
+ * @link          http://www.oxid-esales.com/
+ * @copyright     (C) OXID eSales AG 2003-2015
+ */
+
+
+$sLangName = 'Ru';
+
+$aLang = array(
+    'charset' => 'UTF-8',
+    'oeextendedorderadmin' => 'OE Розширена форма замовлення. Адмін',
+
+    // Order List
+    'OEEXTENDEDORDERADMIN_OXORDERDATE'                                  => 'Дата',
+    'OEEXTENDEDORDERADMIN_HEADER'                                       => 'Сума замовлення',
+    'OEEXTENDEDORDERADMIN_OXARTNUM'                                     => 'Арт.No.',
+    'OEEXTENDEDORDERADMIN_SUM'                                          => 'Кількість',
+    'OEEXTENDEDORDERADMIN_OXTITLE'                                      => 'Описання',
+
+    // Order package
+    'OEEXTENDEDORDERADMIN_TITLE'                                      => 'OXID PACKLIST',
+    'OEEXTENDEDORDERADMIN_SHOWPACKLIST'                               => 'Друк пакувального бланку',
+    'OEEXTENDEDORDERADMIN_PACKLIST'                                   => 'ПАКУВАЛЬНИЙ БЛАНК',
+    'OEEXTENDEDORDERADMIN_FON'                                        => 'Телефон',
+    'OEEXTENDEDORDERADMIN_FAX'                                        => 'Fax',
+    'OEEXTENDEDORDERADMIN_REMARK'                                     => 'Примітка',
+    'OEEXTENDEDORDERADMIN_ORDERNR1'                                   => 'No.',
+    'OEEXTENDEDORDERADMIN_ORDERNR2'                                   => 'Замовлено ',
+    'OEEXTENDEDORDERADMIN_MENUITEM'                                   => 'Адміністрування замовлень',
+    'OEEXTENDEDORDERADMIN_MENUSUBITEM'                                => 'Пакувальний бланк',
+    'OEEXTENDEDORDERADMIN_DETAILS'                                    => 'Деталі',
+);
